efbd7c7bd4fc3dfd02861798fc8624d2a298029b
Mathijs Bernson <mathijsb@q42.nl>
2022-07-29 20:47:18 +0200

    Initial Commit

---
+++ CCCTube/CCCTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCCTubeApp.swift
+//  CCCTube
+//
+//  Created by Mathijs Bernson on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CCCTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCCTube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCCTube
+//
+//  Created by Mathijs Bernson on 29/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
