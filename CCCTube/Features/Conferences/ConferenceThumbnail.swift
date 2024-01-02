53ddca1b65872e029b7b186740b76f0bfea2987c
Mathijs Bernson <mathijsb@q42.nl>
2024-01-02 15:54:06 +0100

    Fix missing images due to request cancellation

--- CCCTube/Features/Conferences/ConferenceThumbnail.swift
+++ CCCTube/Features/Conferences/ConferenceThumbnail.swift
@@ -10,15 +10,23 @@ import SwiftUI
 
 struct ConferenceThumbnail: View {
     let conference: Conference
+    @State private var id: UUID?
 
     var body: some View {
         AsyncImage(url: conference.logoURL) { phase in
             if let image = phase.image {
                 image.resizable().scaledToFit()
-            } else if phase.error != nil {
+            } else if let error = phase.error as? NSError {
                 Text(conference.acronym)
                     .font(.title3.monospaced())
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .onAppear {
+                        // Needed to reload the image when the request has been cancelled.
+                        // This happens when the image scrolls out of view before it has finished loading.
+                        if error.domain == NSURLErrorDomain, error.code == NSURLErrorCancelled {
+                            id = UUID()
+                        }
+                    }
             } else {
                 ProgressView()
             }
@@ -27,6 +35,7 @@ struct ConferenceThumbnail: View {
         .aspectRatio(16 / 9, contentMode: .fit)
         .background(.regularMaterial)
         .cornerRadius(16)
+        .id(id)
     }
 }
 
--- CCCTube/Features/Talk/TalkThumbnail.swift
+++ CCCTube/Features/Talk/TalkThumbnail.swift
@@ -10,22 +10,31 @@ import SwiftUI
 
 struct TalkThumbnail: View {
     let talk: Talk
+    @State private var id: UUID?
 
     var body: some View {
         AsyncImage(url: talk.posterURL ?? talk.thumbURL) { phase in
             if let image = phase.image {
                 image.resizable().scaledToFit()
-            } else if phase.error != nil {
+            } else if let error = phase.error as? NSError {
                 Image(systemName: "xmark.circle")
                     .foregroundStyle(.red)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(.regularMaterial)
+                    .onAppear {
+                        // Needed to reload the image when the request has been cancelled.
+                        // This happens when the image scrolls out of view before it has finished loading.
+                        if error.domain == NSURLErrorDomain, error.code == NSURLErrorCancelled {
+                            id = UUID()
+                        }
+                    }
             } else {
                 ProgressView()
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .aspectRatio(16 / 9, contentMode: .fill)
+        .id(id)
     }
 }
 
