01f9754f2877104e4bb476890dd8a78104fbc647
Mathijs Bernson <mathijsb@q42.nl>
2024-06-14 11:51:03 +0200

    Enable Swift 6.0 language mode with strict concurrency checking

--- CCCApi/Package.swift
+++ CCCApi/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CCCApi",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency=complete")
+            ]
         ),
     ]
 )
